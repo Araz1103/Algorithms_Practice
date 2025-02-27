0ca0fccb0c78f7590e4e327988209f5aa533a708
Araz Sharma <arazsharma1103@gmail.com>
2025-02-28 00:28:42 +0530

    Container water 2 pointer again

--- LeetCode_Probs/Two_Pointers/container_most_water.py
+++ LeetCode_Probs/Two_Pointers/container_most_water.py
@@ -67,3 +67,35 @@ def maxArea(height):
 print(maxArea([1,8,6,2,5,4,8,3,7]))
 print(maxArea([1,1]))
 print(maxArea([3, 2, 3, 5]))
+
+# Same logic, 2 pointers, worded differently
+
+from typing import List
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # Start from L and R
+        # We can calculate Area @any given L and R
+        # Now instead of calculating for all, our aim is to see maximum possible
+        # We know that our area can increase by lenght x width
+        # Length not in control, whenever we shift 1, length decreases by 1
+        # But Width controlled by the bars
+        # The shorter bar is the one which restricts the area
+        # So start L=0 and R@end
+        # Keep calculating area, and updating max area
+        # Move whichever has a shorter bar, as that restricts your area
+
+        L = 0
+        R = len(heights)-1
+        max_area = 0
+
+        while L < R: #Can't be equal as no container possible
+            curr_area = (R-L)*min(heights[L], heights[R])
+            # Update maximum
+            max_area = max(max_area, curr_area)
+            if heights[L] > heights[R]:
+                R-=1
+            else:
+                L+=1
+
+        return max_area
+        
