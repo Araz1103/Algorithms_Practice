eb5a265f872a34b8655ff399868409be8a1a79f9
Araz Sharma <arazsharma1103@gmail.com>
2025-03-08 15:19:05 +0530

    Adding time complexity

--- LeetCode_Probs/Tries/search_word_data_structure.py
+++ LeetCode_Probs/Tries/search_word_data_structure.py
@@ -1,3 +1,44 @@
+"""
+Design a data structure that supports adding new words and searching for existing words.
+
+Implement the WordDictionary class:
+
+void addWord(word) Adds word to the data structure.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. 
+word may contain dots '.' where dots can be matched with any letter.
+Example 1:
+
+Input:
+["WordDictionary", "addWord", "day", "addWord", "bay", "addWord", "may", "search", "say", "search", "day", "search", ".ay", "search", "b.."]
+
+Output:
+[null, null, null, null, false, true, true, true]
+
+Explanation:
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("day");
+wordDictionary.addWord("bay");
+wordDictionary.addWord("may");
+wordDictionary.search("say"); // return false
+wordDictionary.search("day"); // return true
+wordDictionary.search(".ay"); // return true
+wordDictionary.search("b.."); // return true
+Constraints:
+
+1 <= word.length <= 20
+word in addWord consists of lowercase English letters.
+word in search consist of '.' or lowercase English letters.
+
+"""
+
+# Time Complexity
+# To insert words, each letter can be inserted in O(1)
+# So for n letters, it is O(N), n is length of word, #letters
+
+# The time complexity of the search function in the presence of wildcard characters is influenced by 
+# both the number of wildcards and the length of the word. 
+# Specifically, for m wildchards the function may need to explore up to 26^m different branches, 
+# each requiring a traversal of up to n nodes, leading to a worst-case time complexity of O(26^m×n)
 class TrieNode:
     def __init__(self):
         self.children = {}
