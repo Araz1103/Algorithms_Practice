5b509d98ee496883c7a5793e4b2087a42d4298fe
Araz Sharma <arazsharma1103@gmail.com>
2025-03-24 16:29:29 +0530

    Merge Strings Problem

---
+++ LeetCode_Probs/Strings/merge_strings.py
@@ -0,0 +1,47 @@
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
+starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+"""
+
+def mergeAlternately(word1: str, word2: str) -> str:
+        word1_len = len(word1)
+        word2_len = len(word2)
+
+        max_len = min(word1_len, word2_len)
+        merge_word = ""
+        for i in range(max_len):
+            merge_word+=word1[i]
+            merge_word+=word2[i]
+
+        if i == word1_len - 1:
+            merge_word += word2[i+1:]
+        else:
+            merge_word += word1[i+1:]
+
+        return merge_word
