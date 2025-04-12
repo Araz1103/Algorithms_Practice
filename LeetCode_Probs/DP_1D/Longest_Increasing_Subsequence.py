752298fe22ef5c4b1ab096f5d687bf1594bad119
Araz Sharma <arazsharma1103@gmail.com>
2025-04-12 19:43:55 +0530

    LIS Updated

--- LeetCode_Probs/DP_1D/Longest_Increasing_Subsequence.py
+++ LeetCode_Probs/DP_1D/Longest_Increasing_Subsequence.py
@@ -99,4 +99,15 @@ class Solution:
         print(LIS_Map)
         return max(LIS_Map)
 
+# Slightly Shorter and cleaner
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
+
 
