7f55aeb1b5f536f0d1e8b00286a266ce0ac8d717
Araz Sharma <arazsharma1103@gmail.com>
2025-04-15 09:33:29 +0530

    Depth of BT

---
+++ LeetCode_Probs/Trees/max_depth_bst.py
@@ -0,0 +1,46 @@
+from typing import Optional
+"""
+Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its depth.
+
+The depth of a binary tree is defined as the number of nodes along 
+the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+Input: root = [1,2,3,null,null,4]
+Output: 3
+
+Example 2:
+Input: root = []
+Output: 0
+
+Constraints:
+
+0 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.right = right
+        self.left = left
+        
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(curr_node):
+            if not curr_node:
+                return 0 #Depth for Empty Tree is 0, as no nodes
+
+            depth_lst = dfs(curr_node.left)
+            depth_rst = dfs(curr_node.right)
+
+            # Depth is max of left sub tree and right sub tree + 1 for current node
+            return max(depth_lst, depth_rst) + 1
+
+        return dfs(root)
+        
