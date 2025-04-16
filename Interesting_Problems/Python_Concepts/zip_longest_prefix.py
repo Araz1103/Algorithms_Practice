241c944d2a92afe7a161078f6ba250f7cf7f697f
Araz Sharma <arazsharma1103@gmail.com>
2025-04-16 06:28:28 +0530

    Cleaning up

--- Interesting_Problems/Python_Concepts/zip_longest_prefix.py
+++ Interesting_Problems/Python_Concepts/zip_longest_prefix.py
@@ -21,16 +21,18 @@ Constraints:
 strs[i] consists of only lowercase English letters if it is non-empty.
 """
 
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        longest_prefix = ""
-        for letters in zip(*strs): # * to unpack lists and ** to unpack dicts
-            if len(set(letters))==1: #Means all are same
-                longest_prefix += letters[0]
-            else:
-                break #No Need to Check Further
-        return longest_prefix
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    longest_prefix = ""
+    for letters in zip(*strs): # * to unpack lists and ** to unpack dicts
+        print(letters)
+        if len(set(letters))==1: #Means all are same
+            longest_prefix += letters[0]
+        else:
+            break #No Need to Check Further
+    return longest_prefix
+    
+print(longestCommonPrefix(["araz", "ara", "ar"]))
